92a1ef6037623b0d772b1bdf9fc56a1d49e0f75c
Katevan2003 <86622744+Katevan2003@users.noreply.github.com>
2021-08-18 14:26:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function () {
+    $(window).scroll(function () {
+        $('.about').each(function () {
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+650) {
+                $(this).addClass("fadeInLeft");
+            }
+        });
+    });
+})
+
+$(function () {
+    $(window).scroll(function () {
+        $('.portfolio').each(function () {
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+950) {
+                $(this).addClass("fadeInUp");
+            }
+        });
+    });
+})
+
+$(function () {
+    $(window).scroll(function () {
+        $('.consultation').each(function () {
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+1320) {
+                $(this).addClass("bounceIn");
+            }
+        });
+    });
+})
+
+$(document).ready(function() {
+	$('.cta-form').click(function(){
+         header('Location: thank-you.html');
+	});
+});
